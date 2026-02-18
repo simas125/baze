66ab6125a1584cbdcf37364a6606e802b9135f74
simas125 <simas.cedrikas@mif.stud.vu.lt>
2026-02-18 20:38:51 +0200

    prac

--- baze.py
+++ baze.py
@@ -23,16 +23,52 @@
 # for zodis in b:
 #     if zodis == zodis[::-1]:
 #         print(zodis)
-didz = 0
-antras = 0
-sar = [2, 1, 3, 2, 5, 6, 4, 8, 7, 9, 15, 12, 11, 14, 10]
-for i in sar:
-    if i>didz:
-        antras=didz
-        didz=i
-    elif didz>i>antras:
-        antras=i
+# 7 didz = 0
+# antras = 0
+# sar = [2, 1, 3, 2, 5, 6, 4, 8, 7, 9, 15, 12, 11, 14, 10]
+# for i in sar:
+#     if i>didz:
+#         antras=didz
+#         didz=i
+#     elif didz>i>antras:
+#         antras=i
+# print(didz)
+# print(antras)
+# 8 nums = [2, 7, 11, 15]
+# target = 9
+# for i in range(len(nums)):
+#     for j in range(i+1, len(nums)):
+#         if nums[i] + nums[j] == target:
+# seen = {}
+# 9 target = 9
+# nums = [2, 7, 11, 15]
+# for i in range(len(nums)):
+#     reikia = target - nums[i]
+#     if reikia in seen:
+#         print("Sprendimas yra ", reikia, nums[i])
+#     else:
+#         seen[nums[i]] = i
+tikr = {}
+ok = True
+a = input(str("Pirmas zodis: "))
+b = input(str("Antras zodis: "))
+for raide in a:
+    if raide in tikr:
+        tikr[raide] = tikr[raide] + 1
+    else:
+        tikr[raide] = 1
 
-    
-print(didz)
-print(antras)
+for raide in b:
+    if raide in tikr:
+        tikr[raide] = tikr[raide] - 1
+    else:
+        ok = False
+        break
+for kiekis in tikr.values():
+    if kiekis != 0:
+        ok = False
+        break
+if ok:
+    print("Anagrama")
+else:
+    print("ne anagrama")
