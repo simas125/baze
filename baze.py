216867100de127a355e7b4b8c334f6e476d36cb3
simas125 <simas.cedrikas@mif.stud.vu.lt>
2026-02-18 18:40:30 +0200

    baze

---
+++ baze.py
@@ -0,0 +1,25 @@
+# 1 for i in range(10, 0, -1):
+#    print(i)**
+# 2 for i in range(2, 21, 2):
+#     print(i)
+# 3 a = int(input("tavo n: "))
+# suma=0
+# for i in range(1, a + 1):
+#     suma+=i
+# print(suma)
+# 4 a = [3, 7, 2, 9, 5, 99]
+# did = a[0]
+# for elementas in a:
+#     if elementas > did:
+#         did = elementas
+# print(did)
+# 5 a = [1, 2, 3, -1, -2, 0, 5, -5]
+# suma = 0
+# for i in a:
+#     if i>0:
+#         suma = suma + 1
+# print(suma)
+# 6 b = ("radar", "anna", "python", "12321")
+# for zodis in b:
+#     if zodis == zodis[::-1]:
+#         print(zodis)
