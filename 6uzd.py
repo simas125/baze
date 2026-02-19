6cea7eab48c8957a5f6b4321753d03376bf995ab
simas125 <simas.cedrikas@mif.stud.vu.lt>
2026-02-19 17:42:17 +0200

    return pair true/false

---
+++ 6uzd.py
@@ -0,0 +1,7 @@
+def hasPair(nums, target):
+    seen = {}
+    for i in range(len(nums)):
+        reikia = target - nums[i]
+        if reikia in seen:
+            return(True)
+        seen[nums]= i
