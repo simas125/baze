99932279c1462b59cdc1c4d2955487c5e8beae89
simas125 <simas.cedrikas@mif.stud.vu.lt>
2026-02-19 10:03:55 +0200

    kartojimas

---
+++ kartot.py
@@ -0,0 +1,30 @@
+# s =[1, 2, 3, 4, 6]
+# lyg = 0
+# for i in s:
+#     if i%2==0:
+#         lyg = lyg + 1
+# print(lyg)
+# 2222 nums = [1, 1, 2, 3, 3, 4]
+# a = list(set(nums))
+# print(a)
+# 33333allowed = {2, 4, 6, 8}
+# x = 5
+# if x in allowed:
+#     print("YES")
+# else:
+#     print("NO")
+# 4444nums = [7, 2, 9, 1]
+# didz = nums[0]
+# for i in nums:
+#     if i > didz:
+#         didz = i
+# print(didz)
+nums = [1, 2, 5, 4, 3]
+def findMax(nums):
+    didz = nums[0]
+    for i in nums:
+        if i > didz:
+            didz = i
+    return didz
+x = didz
+print (x)
