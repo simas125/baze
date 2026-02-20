5cfe6ae84511c939503d8f6b033ef4d43b927e45
simas125 <simas.cedrikas@mif.stud.vu.lt>
2026-02-20 17:00:58 +0200

    stack

--- 8uzd.py
+++ 8uzd.py
@@ -1,14 +1,11 @@
 def first_nr(s):
     seen = {}
-    for i in range(len(s)):
+    for i in s:
         if i in seen:
             seen[i] += 1
         else:
             seen[i] = 1
-    return seen
-    for i in seen:
+    for i in s:
         if seen[i] == 1:
-            print(i)
-            break
-        else:
-            print(None)
+            return(i)
+    return None
